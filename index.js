7d9e4a425dd02f7adbc6318be5846b1a59283a24
Minh Tri <dangvantri250596@gmail.com>
2017-04-14 20:50:19 +0700

    add basic server features

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
 
+//
+app.use(express.static('public'));
+app.use('/components', express.static('bower_components'))
 var port = 8080;
 app.listen(port, function () {
   console.log('Example app listening on port' + port + '!');
